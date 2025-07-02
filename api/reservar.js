28c612abbe253dcf633425747c24e2104154c32a
ManelocoXD <manelmecano@gmail.com>
2025-07-02 17:40:37 +0200

    correccion

--- api/reservar.js
+++ api/reservar.js
@@ -1,4 +1,4 @@
-import mysql from "mysql2/promise";
+const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,23 +7,23 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== "POST") return res.status(405).end("Método no permitido");
 
   const { nombre, telefono, personas, hora, fecha } = req.body;
 
   if (!nombre || !telefono || !personas || !hora || !fecha) {
-    return res.status(400).json({ error: 'Faltan datos obligatorios' });
+    return res.status(400).json({ error: "Faltan datos obligatorios" });
   }
 
   try {
     await db.execute(
-      'INSERT INTO reservas (nombre, telefono, personas, hora, fecha) VALUES (?, ?, ?, ?, ?, ?)',
-      [nombre, telefono, personas, hora, fecha]
+      "INSERT INTO reservas (nombre, telefono, personas, hora, fecha, estado) VALUES (?, ?, ?, ?, ?, ?)",
+      [nombre, telefono, personas, hora, fecha, "pendiente"]
     );
     res.status(200).send("Reserva guardada");
   } catch (err) {
     console.error("Error al guardar reserva:", err);
     res.status(500).send("Error en la base de datos");
   }
-}
+};
