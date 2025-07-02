242f531691cf6aa9f8ceb5b945f547c0820cbc50
ManelocoXD <manelmecano@gmail.com>
2025-07-02 17:08:22 +0200

    correccion base de datos

--- api/reservar.js
+++ api/reservar.js
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
 
   try {
     await db.execute(
-      'INSERT INTO reservas (nombre, telefono, personas, hora, fecha, estado) VALUES (?, ?, ?, ?, ?, ?)',
-      [nombre, telefono, personas, hora, fecha, 'pendiente']
+      'INSERT INTO reservas (nombre, telefono, personas, hora, fecha) VALUES (?, ?, ?, ?, ?, ?)',
+      [nombre, telefono, personas, hora, fecha]
     );
     res.status(200).send("Reserva guardada");
   } catch (err) {
